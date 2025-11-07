0bf49623920b11775971cb931f4704ebe91d256a
ZinedineHMC <zinehiramc@outlook.com>
2025-11-07 08:19:00 -0600

    Borrado de archivos inutiles

--- src/config/mail.php
+++ src/config/mail.php
@@ -4,7 +4,7 @@
 return [
     'host' => 'smtp.sendgrid.net',
     'username' => 'apikey', // Este valor es siempre 'apikey' para SendGrid
-    'password' => '*', //* Tu clave de API de SendGrid
+    'password' => '#', // Tu clave de API de SendGrid
     'port' => 587,
     'secure' => 'tls', // 'tls' o 'ssl'
     'from_email' => 'prisma_pos@outlook.com', // El correo que verificaste en SendGrid
