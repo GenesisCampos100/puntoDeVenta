50056d3eb69d0a9998c1b074971213b372f945d6
ZinedineHMC <zinehiramc@outlook.com>
2025-11-24 18:01:21 -0600

    Mejora en la seguridad del archivo de config

--- src/config/mail.php
+++ src/config/mail.php
@@ -1,10 +1,13 @@
 <?php
+require_once __DIR__ . '/../../vendor/autoload.php'; // Ajusta la ruta si estás en src/config/
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__ . '/../../'); // Ruta al .env
+$dotenv->load();
 // Configuración SMTP para PHPMailer
 // Configuración para SendGrid
 return [
     'host' => 'smtp.sendgrid.net',
     'username' => 'apikey', // Este valor es siempre 'apikey' para SendGrid
-    'password' => '#', // Tu clave de API de SendGrid
+'password' => $_ENV['API_KEY'], // Cargado desde .env
     'port' => 587,
     'secure' => 'tls', // 'tls' o 'ssl'
     'from_email' => 'prisma_pos@outlook.com', // El correo que verificaste en SendGrid
